d41c7083315660dbc240297166e6d248b763cc70
Seynabou12 <dioneseynabou0@gmail.com>
2022-07-31 22:52:20 +0000

    modification des status

--- resources/views/mentores/mentors.blade.php
+++ resources/views/mentores/mentors.blade.php
@@ -37,7 +37,8 @@
                                         </td>
                                         <td><span class="text-black">{{ $connexion->mentor->user->email }}</span></td>
                                         <td><span class="text-black">{{ $connexion->mentor->user->telephone }} </span></td>
-                                        <td><span class="text-black">{{ $connexion->status }} </span></td>
+                                        <td><span class="badge light badge-danger">{{ $connexion->status }}</span></td>
+                                        {{-- <td><span class="text-black">{{ $connexion->status }} </span></td> --}}
                                         <td>
                                             <div class="dropdown">
                                                 <a href="javascript:void(0);" class="btn-link" data-bs-toggle="dropdown"
